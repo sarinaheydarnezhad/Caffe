36ddf22b53b86d29fea4b370bf0e78dbbd050533
sarinaheydarnezhad <sarinaheydarnejad1383@gmail.com>
2026-08-28 12:39:54 +0330

    Implement basic cafe booking server with Express

---
+++ script.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const bookings = [];
+
+app.get('/', (req, res) => {
+  res.send('Cafe booking server is running!');
+});
+
+app.post('/booking', (req, res) => {
+  const { name, phone, date, time, guests } = req.body;
+
+  const newBooking = { name, phone, date, time, guests };
+  bookings.push(newBooking);
+
+  console.log('New booking received:', newBooking);
+
+  res.send('Thanks ' + name + ', your booking is confirmed!');
+});
+
+app.get('/bookings', (req, res) => {
+  res.json(bookings);
+});
+
+app.listen(3000, () => {
+  console.log('Cafe booking server is running on http://localhost:3000');
+});
